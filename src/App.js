6cad735504a412e85265da95b75e009f34f24adb
Shaine Ingamells <shaineingamells@Shaines-MacBook-Pro.local>
2019-10-18 09:27:21 -0600

    app.js and charactercard.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ export default function App() {
   return (
     <main>
       <Header />
+      <Link to="/" >Home</Link>
+      <Link to="/characters" >Characters</Link>
+      <Route exact path="/" component={WelcomePage}/>
+      <Route exact path="/characters" component={CharacterList}/>
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({name, image}) {
+  return (
+    <div>
+      <p>Name: {name}</p>
+      <img src={image} alt="Rick and Morty character picture"/>
+    </div>
+  );
 }
